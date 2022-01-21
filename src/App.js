d072dc7a63e6db8bc1e39f7ae160d5f37944d536
amabelle <amabelletr@gmail.com>
2022-01-21 03:42:56 +0200

    Display Population table & styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import PopulationTable from './components/population-table/PopulationTable';
+import Emoji from './components/emoji/Emoji';
 
 import './App.css';
 
 function App() {
   return (
     <>
-      <h1>Star Wars</h1>
+      <h1 className="header center">
+        <Emoji symbol="🚀" label="rocket" />
+        <Emoji symbol="👾" label="space-invader" />
+        Star Wars API
+        <Emoji symbol="👽" label="alien" />
+        <Emoji symbol="🚀" label="rocket" />
+      </h1>
       <PopulationTable />
     </>
   );
--- src/components/population-table/PopulationTable.js
+++ src/components/population-table/PopulationTable.js
@@ -5,15 +5,46 @@ import './PopulationTable.css';
 
 const PopulationTable = () => {
   const { state } = useStarWarsFetch();
+  const { vehicles, max } = state;
   console.log(
     '🚀 ~ file: PopulationTable.js ~ line 9 ~ PopulationTable ~ state',
     state
   );
 
   return (
-    <div>
-      <h1>PopulationTable</h1>
-    </div>
+    <>
+      {max && (
+        <div className="max-population-vehicle_table">
+          <div className="table-row">
+            <div className="cell pad">Vehicle name with the largest sum:</div>{' '}
+            <div className="col cell grow">{state.max.name}</div>
+          </div>
+          <div className="table-row">
+            <div className="cell pad">
+              Related home planets and their respective population:
+            </div>
+            <div className="col cell grow">
+              {state.max.planets.map((planet, i) => (
+                <span className="row" key={i}>
+                  <p>{planet.data.name}:</p>
+                  <p>{planet.data.population}</p>
+                </span>
+              ))}
+            </div>
+          </div>
+          <div className="table-row">
+            <div className="cell pad"> Related pilot names:</div>
+            <div className="col cell grow">
+              {state.max.fetchedPilots.map((pilot, i) => (
+                <span key={i} className="row">
+                  {pilot.data.name}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
