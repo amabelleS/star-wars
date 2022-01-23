a6d2f227ec30d7077076d4e49ab98106d62c83d1
amabelle <amabelletr@gmail.com>
2022-01-23 14:13:55 +0200

    Prev commit

--- src/components/planets-chart/PlanetsChart.js
+++ src/components/planets-chart/PlanetsChart.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSwapiPlanetsFetch } from '../../hooks/useSwapiPlanetsFetch';
-// import BarChart from '../bar-chart';
-import BarChart from '../BarChart';
+import BarChart from '../bar-chart';
 
 const PlanetsChart = () => {
   // const {
